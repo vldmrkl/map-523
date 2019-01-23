68975e2f9b4de20c722802d61c97a0267636cf3c
Volodymyr <klymenkodp@gmail.com>
2019-01-23 08:31:16 -0500

    Initialize Lab 2 project

---
+++ Lab2/Lab2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab2
+//
+//  Created by Volodymyr Klymenko on 2019-01-23.
+//  Copyright © 2019 Volodymyr Klymenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
