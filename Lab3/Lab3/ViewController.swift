0a915927c458ba06642052d52478d39a9001c60d
Volodymyr <klymenkodp@gmail.com>
2019-01-30 14:57:00 -0500

    Add game state

--- Lab3/Lab3/ViewController.swift
+++ Lab3/Lab3/ViewController.swift
@@ -11,13 +11,18 @@ import UIKit
 class ViewController: UIViewController {
 
 	@IBOutlet weak var resultLabel: UILabel!
-	
+	@IBOutlet weak var newGameButton: UIButton!
+	@IBOutlet weak var saveGameButton: UIButton!
+	@IBOutlet weak var loadGameButton: UIButton!
+
+	@IBOutlet var buttons: [UIButton]!
+
 	var gameOver = false
-    
-    var turn = 1    //1 is O  2 is X
-    
+	var turn = 1    //1 is O  2 is X
     var board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    
+	var gameStateBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+	var gameStateTurn = 1
+    var gameStateGameOver = false
     var winning_conditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     
     override func viewDidLoad() {
@@ -45,26 +50,63 @@ class ViewController: UIViewController {
                 turn = 1
             }
         
-        for win_cond in winning_conditions{
-            
-            if board[win_cond[0]] != 0 && board[win_cond[0]] == board[win_cond[1]] && board[win_cond[1]] == board[win_cond[2]]
-            {
-				if turn == 1 {
-					resultLabel.text = "Xs won"
+			for win_cond in winning_conditions{
+
+				if board[win_cond[0]] != 0 && board[win_cond[0]] == board[win_cond[1]] && board[win_cond[1]] == board[win_cond[2]]
+				{
+					if turn == 1 {
+						resultLabel.text = "Xs won"
+					}
+					else{
+						resultLabel.text = "Os won"
+					}
+					gameOver = true
 				}
-				else{
-					resultLabel.text = "Os won"
+				else if !board.contains(0){
+					gameOver = true
+					resultLabel.text = "Draw"
 				}
-                gameOver = true
-            }
-			else if !board.contains(0){
-				gameOver = true
-				resultLabel.text = "Draw"
 			}
-        }
-        
-            print(board)
-        }
+		}
     }
+
+	@IBAction func newGame(_ sender: Any) {
+		resultLabel.text = ""
+		board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+		gameOver = false
+		for i in 0..<board.count {
+			switch(board[i]){
+			case 1:
+				buttons[i].setImage(UIImage(named: "nought.png"), for: [])
+			case 2:
+				buttons[i].setImage(UIImage(named: "cross.png"), for: [])
+			default:
+				buttons[i].setImage(nil, for: [])
+			}
+		}
+	}
+
+	@IBAction func saveGame(_ sender: Any) {
+		gameStateBoard = board
+		gameStateTurn = turn
+		gameStateGameOver = gameOver
+	}
+
+	@IBAction func loadGame(_ sender: Any) {
+		gameOver = false
+		board = gameStateBoard
+		turn = gameStateTurn
+		gameOver = gameStateGameOver
+		for i in 0..<board.count {
+			switch(board[i]){
+				case 1:
+					buttons[i].setImage(UIImage(named: "nought.png"), for: [])
+				case 2:
+					buttons[i].setImage(UIImage(named: "cross.png"), for: [])
+				default:
+					buttons[i].setImage(nil, for: [])
+			}
+		}
+	}
 }
 
