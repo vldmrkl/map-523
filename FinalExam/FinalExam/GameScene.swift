4988e10023bcce853cbbeafa81334d828e7874b5
Volodymyr <klymenkodp@gmail.com>
2019-04-10 08:38:38 -0400

    Add letters spawning and walls

--- FinalExam/FinalExam/GameScene.swift
+++ FinalExam/FinalExam/GameScene.swift
@@ -10,80 +10,75 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+
+    var ABCGate: SKSpriteNode?
+    var DEFGate: SKSpriteNode?
+    var GHIGate: SKSpriteNode?
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var scoreLabel : SKLabelNode!
+    var score: Int = 0
+
+    var letterTimer: Timer?
+
+    let characterCategory: UInt32 = 0x1 << 0
+    let abcCategory: UInt32 = 0x1 << 1
+    let defCategory: UInt32 = 0x1 << 2
+    let ghiCategory: UInt32 = 0x1 << 3
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        ABCGate = childNode(withName: "ABCGate") as? SKSpriteNode
+        ABCGate?.physicsBody?.categoryBitMask = abcCategory
+        ABCGate?.physicsBody?.collisionBitMask = abcCategory
+        ABCGate?.physicsBody?.contactTestBitMask = characterCategory
+
+        DEFGate = childNode(withName: "DEFGate") as? SKSpriteNode
+        DEFGate?.physicsBody?.categoryBitMask = defCategory
+        DEFGate?.physicsBody?.collisionBitMask = defCategory
+        DEFGate?.physicsBody?.contactTestBitMask = characterCategory
+
+        GHIGate = childNode(withName: "GHIGate") as? SKSpriteNode
+        GHIGate?.physicsBody?.categoryBitMask = ghiCategory
+        GHIGate?.physicsBody?.collisionBitMask = ghiCategory
+        GHIGate?.physicsBody?.contactTestBitMask = characterCategory
+
+        scoreLabel = childNode(withName: "scoreLabel") as? SKLabelNode
+        scoreLabel.text = "Score: \(score)"
+
+        letterTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (timer) in
+            self.spawnCharacter()
+        })
+
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+
+    func spawnCharacter() {
+        let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+        let randomCharacter = letters.randomElement()!
+        let character = SKSpriteNode(imageNamed: randomCharacter)
+
+        character.size = CGSize(width: 75, height: 75)
+        character.physicsBody = SKPhysicsBody(rectangleOf: character.size)
+
+        character.physicsBody?.affectedByGravity = false
+        character.physicsBody?.isDynamic = true
+
+        character.physicsBody?.categoryBitMask = characterCategory
+//        character.physicsBody?.contactTestBitMask = rocketCategory | spaceshipCategory
+//        character.physicsBody?.collisionBitMask = 0
+
+        character.name = "character\(randomCharacter.uppercased())Node"
+
+        self.addChild(character)
+
+        let minX = -size.width/2 + character.size.width
+        let maxX = size.width/2 - character.size.width
+        let range = maxX - minX
+        let randomCharX = maxX - CGFloat(arc4random_uniform(UInt32(range)))
+
+        character.position = CGPoint(x: randomCharX, y: size.height / 2 + character.size.height / 2)
+
+        let flyDown = SKAction.moveBy(x: 0, y: -size.height - character.size.height, duration: 4)
+        let moveUFO = SKAction.sequence([flyDown, SKAction.removeFromParent()])
+
+        character.run(moveUFO)
     }
 }
